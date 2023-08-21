ec5b66fa788819b75f04130fdb12f033b8aada01
cluelin <cluelin@gmail.com>
2023-08-22 00:50:28 +0900

    Modify MainActivity3 -> MainActivity

--- app/src/main/java/com/cluelin/app/MainActivity.kt
+++ app/src/main/java/com/cluelin/app/MainActivity.kt
@@ -1,52 +1,62 @@
 package com.cluelin.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.cluelin.app.Fragment.QuestGenerator
+import com.cluelin.app.Fragment.QuestList
+import com.cluelin.app.Fragment.QuestLogFragment
+import com.google.android.material.navigation.NavigationBarView
 import com.cluelin.app.db.DataManager
-import com.cluelin.app.db.Quest
 
 class MainActivity : AppCompatActivity() {
+
+    var questListFragment = QuestList()
+    var questLogFragment = QuestLogFragment()
+    var questGenerator = QuestGenerator()
+    private lateinit var sharedViewModel: SharedViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val TAG = "FORLOG"
+        setViewModel()
+        setFragment()
+    }
 
+    private fun setViewModel(){
+        sharedViewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+        sharedViewModel.dataManager = DataManager(this)
 
-        val questBtn = findViewById<Button>(R.id.display_quest)
-        // To insert a new note
-        val dataManager = DataManager(this.applicationContext)
+        val questList = sharedViewModel.dataManager.getAllUnDoneQuests()
+        sharedViewModel.questAdapter = QuestAdapter(questList, questListFragment)
+    }
 
-        questBtn.setOnClickListener {
-            // To retrieve all notes
-            val quests = dataManager.getAllQuests()
-            Log.e(TAG, "클릭 시작")
-            Log.e(TAG, quests.toString())
+    private fun setFragment(){
+        supportFragmentManager.beginTransaction().replace(R.id.containers, questListFragment).commit()
 
-
-            var unDoneQuests = dataManager.getAllUnDoneQuests()
-            Log.e(TAG, unDoneQuests.toString())
-
-            dataManager.updateCompletedTime("Daily Quest", "2023-08-07 8:07:22")
-            dataManager.updateCompletedTime("Daily Quest", "2023-08-08 9:07:22")
-
-            unDoneQuests = dataManager.getAllUnDoneQuests()
-            Log.e(TAG, unDoneQuests.toString())
-
-
-            val questLogs = dataManager.getQuestLogListByTitle("Daily Quest")
-            Log.e(TAG, questLogs.toString())
-
-//            dataManager.removeAllRows()
-
+        var navigation: NavigationBarView = findViewById(R.id.bottom_navigation_view)
+        navigation.setOnItemSelectedListener { item ->
+            rotateFragments(item)
         }
+    }
 
-
-        dataManager.insertQuest(Quest("Daily Quest", 1, "2023-08-06 10:07:22"))
-        dataManager.insertQuest(Quest("Week Quest", 7, "2023-08-07 10:07:22"))
-
-
+    private fun rotateFragments(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.containers, questListFragment).commit()
+                return true
+            }
+            R.id.log -> {
+                supportFragmentManager.beginTransaction().replace(R.id.containers, questLogFragment).commit()
+                return true
+            }
+            R.id.add -> {
+                supportFragmentManager.beginTransaction().replace(R.id.containers, questGenerator).commit()
+                return true
+            }
+            else -> return false
+        }
     }
 }
